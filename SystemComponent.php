0adc6b70b8b9b3afb0611641ee5c1ce268893a1e
Jose Scarnatto <jmscarnatto@gmail.com>
2020-06-14 21:34:22 -0300

    Create SystemComponent.php

---
+++ SystemComponent.php
@@ -0,0 +1,23 @@
+<?php
+class SystemComponent {
+
+	Var $settings;
+	Function getSettings(){
+	$settings['siteDir']='<fake-path>';
+	$settings['dbhost']='ip-of-host';
+	$settings['dbuser']='root';
+	$settings['dbpassword']='root_password';
+	$settings['dbname']='db-name';
+	$settings['hash']='sha1';
+	return $settings;
+	}
+}
+define('APP_PATH','<fake>/rsaportal/cpt/');
+define('PPD_PATH', APP_PATH.'xml/ppd_data.xml');
+define('PKD_PATH', APP_PATH.'xml/pkd.xml');
+define('PPDT_PATH', APP_PATH.'xml/pkd_temp.xml');
+define('OBJETIVES_PATH', APP_PATH.'xml/objetives.xml');
+define('FERIADOS_PATH', APP_PATH.'xml/feriados.xml');
+define('FILES_ADDR', 'files/');
+
+?>
